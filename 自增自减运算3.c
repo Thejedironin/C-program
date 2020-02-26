4d86a8ff69ca15fe8242cd14aec5d6e555f06804
Thejedironin <61299536+Thejedironin@users.noreply.github.com>
2020-02-26 10:29:34 +0800

    日常上传

---
+++ 自增自减运算3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+	int i=6, a, b;
+	
+	printf("%d\n", ++i);
+	printf("%d\n", i++);
+	
+	a=--i; printf("%d\n", a);
+	b=i--, printf("%d\n", b);
+	
+	printf("%d\n",-i++);//œ»À„-i£¨‘Ÿ++ 
+	printf("i=%d\n", i);
+	return 0;
+}
